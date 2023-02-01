673bbf484048b889dc90df917beab5e7033eb6b1
mjabulani <marekzmienka@gmail.com>
2023-02-01 11:58:23 +0100

    initial commit with structure of Main

---
+++ src/com/marek/zmienka/fun/A1.java
@@ -0,0 +1,11 @@
+package com.marek.zmienka.fun;
+
+public class A1 {
+
+    public static void a1() {
+        System.out.println("A1 chosen");
+    }
+    public static void a2() {
+        System.out.println("A1 chosen");
+    }
+}
--- src/com/marek/zmienka/fun/Main.java
+++ src/com/marek/zmienka/fun/Main.java
@@ -1,8 +1,19 @@
 package com.marek.zmienka.fun;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Cycki");
+    public static void main(String[] args) throws Exception {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Select program to run:");
+        int programNumber = s.nextInt();
+        switch (programNumber) {
+            case 1: A1.a1();
+            default: throw new Exception("Wrong choice");
+        }
     }
 }
+
