9bbe8363252e77b16848650ee6aaadfac3187513
mjabulani <marekzmienka@gmail.com>
2023-02-13 19:35:05 +0100

    Alg1

--- src/com/marek/zmienka/fun/A1.java
+++ src/com/marek/zmienka/fun/A1.java
@@ -1,9 +1,64 @@
 package com.marek.zmienka.fun;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class A1 {
 
     public static String STRING_CHARS = "asdfghjklqwertyuiozxcvbnm";
-    public static String INT_CHARS = "0123456789";
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> arrayStr = generateStringArray();
+        ArrayList<Integer> arrayInt = generateArray();
+
+        System.out.println("Unsorted array of ints: " + arrayInt);
+        System.out.println("Unsorted array of strings: " + arrayStr);
+
+        Collections.sort(arrayInt);
+        Collections.sort(arrayStr);
+
+        System.out.println("Sorted array of ints: " + arrayInt);
+        System.out.println("Sorted array of strings: " + arrayStr);
+
+    }
+
+
+    public static ArrayList<Integer> generateArray() {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        Random rand = new Random();
+
+        while (array.size() < 10) {
+            int temp = rand.nextInt(500);
+            if (!array.contains(temp)) {
+                array.add(temp);
+            }
+        }
+        return array;
+    }
+
+    public static ArrayList<String> generateStringArray() {
+        ArrayList<String> array = new ArrayList<String>();
+        Random rand = new Random();
+        while (array.size() < 10) {
+            StringBuilder tempString = generateString();
+            String s = tempString.toString();
+            array.add(s);
+        }
+        return array;
+    }
+
+    public static StringBuilder generateString() {
+        StringBuilder string = new StringBuilder();
+        Random rand = new Random();
+        for (int i = 0; i < 8; i++) {
+            string.append(STRING_CHARS.charAt(rand.nextInt(STRING_CHARS.length())));
+        }
+        return string;
+    }
+
 
 
 }
--- src/com/marek/zmienka/fun/Main.java
+++ src/com/marek/zmienka/fun/Main.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutionException;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        
+
     }
 
     public static void runProgram() throws Exception {
