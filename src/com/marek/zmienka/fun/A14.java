2a250755a11a5b04a2d81f0cd9a083a61d33c2eb
mjabulani <marekzmienka@gmail.com>
2023-02-14 13:41:57 +0100

    A14 - completed

--- src/com/marek/zmienka/fun/A14.java
+++ src/com/marek/zmienka/fun/A14.java
@@ -13,7 +13,7 @@ public class A14 {
 
         System.out.println("#### 14. Program to find common elementos of two generated arrays of int");
 
-        ArrayList<Integer> array1 = A4.generateArrayNoOccurances(10, 20, 40);
+        ArrayList<Integer> array1 = A4.generateArrayNoOccurances(8, 20, 40);
         ArrayList<Integer> array2 = A4.generateArrayNoOccurances(8, 21, 54);
 
         Collections.sort(array1);
@@ -27,9 +27,18 @@ public class A14 {
     public static ArrayList<Integer> commonElements(ArrayList<Integer> array1, ArrayList<Integer> array2) {
         ArrayList<Integer> common = new ArrayList<Integer>();
 
-        for (Integer integer : array2) {
-            if (array1.contains(integer)) {
-                common.add(integer);
+        if (array1.size() >= array2.size()) {
+            for (Integer integer : array2) {
+                if (array1.contains(integer)) {
+                    common.add(integer);
+                }
+            }
+
+        } else {
+            for (Integer integer : array1) {
+                if (array2.contains(integer)) {
+                    common.add(integer);
+                }
             }
         }
 
